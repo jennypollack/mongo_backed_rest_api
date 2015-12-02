d89d57983d8834b7f802b7351008a9407fcea95f
Jenny Pollack <JennyPollack@Jennys-MacBook-Pro.local>
2015-12-01 18:46:09 -0800

    add beginning of angular app structure

--- jenny_pollack/gulpfile.js
+++ jenny_pollack/gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var mocha = require('gulp-mocha');
+var webpack = require('webpack-stream');
 var appFiles = ['index.js', 'lib/**/*.js', 'bin/**/*.js', 'models/**/*.js', 'routes/**/*.js'];
 var testFiles = ['./test/**/*.js'];
 
@@ -38,6 +39,21 @@ gulp.task('mocha', function(){
     .pipe(mocha({reporter: 'spec'}));
 });
 
+gulp.task('static:dev', function() {
+  gulp.src('app/**/*.html')
+  .pipe(gulp.dest('build/'));
+});
 
+gulp.task('webpack:dev', function() {
+  return gulp.src('app/js/entry.js')
+  .pipe(webpack({
+    output: {
+      filename: 'bundle.js'
+    }
+  }))
+  .pipe(gulp.dest('build/'));
+});
+
+gulp.task('build:dev', ['webpack:dev', 'static:dev']);
 gulp.task('jshint', ['jshint:test', 'jshint:app']);
-gulp.task('default', ['jshint', 'mocha']);
+gulp.task('default', ['jshint', 'mocha', 'build:dev']);
