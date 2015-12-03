fb347829cab5d1c07b2c92c68756ab1a32d19369
Jenny Pollack <JennyPollack@Jennys-MacBook-Pro.local>
2015-12-02 16:44:37 -0800

    wat

--- jenny_pollack/app/js/entry.js
+++ jenny_pollack/app/js/entry.js
@@ -2,5 +2,5 @@ require('angular/angular');
 var angular = window.angular;
 
 var songsApp = angular.module('SongsApp', []);
-//require('./controllers/controllers')(bearStreamApp);
+//require('./controlalers/controllers')(bearStreamApp);
 require('./songs/songs')(songsApp);
